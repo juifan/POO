97dfb8635296193169891fbc5bbefce6ef703b35
cristian <cristian_perez_1996@hotmail.com>
2023-05-24 21:13:14 -0600

    add class user and driver

--- Python/main.py
+++ Python/main.py
@@ -1,7 +1,7 @@
 from car import Car
 from account import Account
 from uberX import UberX
-
+from user import User
 
 if __name__ == "__main__":
 
@@ -12,3 +12,7 @@ if __name__ == "__main__":
     uberX =  UberX("ERQ352",  Account("Andrea", "Anda56"), "Chevrolet", "Aveo")
     print(vars(uberX))
     print(vars(uberX.drive))
+
+    user = User("Cris","ERT361")
+    print(vars(user))
+    
